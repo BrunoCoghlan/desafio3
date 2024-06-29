576b77820455f03a836f7d587bddf375523072db
Bruno Coghlan <brunocoghlan@gmail.com>
2024-06-29 13:42:58 -0400

    DML modify

--- database/DML.sql
+++ database/DML.sql
@@ -1,7 +1,7 @@
 -- datos respaldados.
 
-INSERT INTO post (id, titulo, img, descripcion, likes) VALUES
+INSERT INTO public.posts (titulo,img,descripcion,likes) VALUES
+	 ('Guaripolo 1','https://i.pinimg.com/originals/83/b1/da/83b1da82c65a6ce279b1a3153639ff17.jpg','Guaripolo 1 test',0),
+	 ('Juanin Juan Harry','https://31minutosoficial.cl/w/juaninsite/1516915_1418220001772036_743573176_n.jpg','Juanin Juan Harry test 2',0),
+	 ('Carlitos Lechuga','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSSRJwcu3aqXYo4Zu_HPF_rvkAQJ9WJmNGd3w&s','Carlitos Lechuga test 3',0);
 
-
-
-SELECT * FROM posts
